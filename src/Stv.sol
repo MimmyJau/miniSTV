fd1b9b64d7f3d8b763abb7a7f763311ab8396d82
Jimmy <jimmy.b.mau@gmail.com>
2023-11-09 18:08:17 -0500

    feat: WIP untested voting functions

--- src/Stv.sol
+++ src/Stv.sol
@@ -1,107 +1,86 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Tree} from "../src/Tree.sol";
-
-// Possible options for proposal status
-enum Status {
-    Eliminated,
-    Winner,
-    Undecided
-}
-
-// Proposal object
-struct Proposal {
-    bytes32 name; // Only allow names of 32 chars or less.
-    uint256 tally;
-    Status status;
-}
-
-struct VoteTree {
-    uint256 proposal;
-    uint256 votes;
-    uint256 cumulativeVotes;
-}
+import {RankVote} from "../src/RankVote.sol";
 
 // Voter object
+// Might not have to hold vote in storage
 struct Voter {
     bool voted;
-    uint weight;
-    uint8 vote1; // Limit to at most 256 proposals, so use uint8.
-    uint8 vote2; // Might not actually need to hold this data in storage.
-    uint8 vote3;
+    uint256 weight;
+    uint256[] votes; 
 }
 
-
-contract Stv is Tree {
-    // Public state 
-    Proposal[] public proposals;
-    // mapping(bytes32 => VoteTree) public VoteTree;
-    mapping(address => Voter) public voters; 
+contract Stv is RankVote {
     address public chairperson;
+    bytes32[] public proposals;
+    bool private _active;
+    uint256[] private _winners;
+    mapping(address => Voter) private _voters; 
 
-    // Errors
-    error OnlyChairperson();
-    error SenderAlreadyHasRightToVote();
-    error SenderAlreadyVoted();
-    error SenderHasNoRightToVote();
-    error SubmittedMoreThanThreeVotes();
-    error TooManyProposals(uint256 size);
-
-    // Proposal constructor
-    constructor(bytes32[] memory proposalNames) {
-        if (proposalNames.length > 256) revert TooManyProposals(proposalNames.length);
+    constructor(bytes32[] memory proposalNames) RankVote(proposalNames.length) {
+        require(proposalNames.length < 256);
 
         chairperson = msg.sender;
-        voters[chairperson].weight = 1;
+        _voters[chairperson].weight = 1;
 
         for (uint8 i = 0; i < proposalNames.length; i++) {
-            proposals.push(Proposal({
-                name: proposalNames[i],
-                tally: 0,
-                status: Status.Undecided
-            }));
+            proposals.push(proposalNames[i]);
         }
     }
 
-    // Grant voting power
-    function grantRightToVote(address aspiringVoter) public {
-        if (msg.sender != chairperson) revert OnlyChairperson();
-        // Q: Does indexing voters[aspiringVoter] each time cost more gas 
-        //    vs. setting Voter messager voter = voters[aspiringVoter]?
-        if (voters[aspiringVoter].voted) revert SenderAlreadyVoted();
-        if (voters[aspiringVoter].weight != 0) revert SenderAlreadyHasRightToVote();
+    function suffrage(address aspiringVoter) external {
+        require(msg.sender == chairperson);
+        require(!_voters[aspiringVoter].voted);
+        require(_voters[aspiringVoter].weight == 0);
+        require(_active);
 
-        voters[aspiringVoter].weight = 1;
+        _voters[aspiringVoter].weight = 1;
     }
 
-    // Vote
-    // function vote(uint8[] calldata ballot) external {
-    //     if (ballot.length > 3) revert SubmittedMoreThanThreeVotes();
-    //     if (voters[msg.sender].weight == 0) revert SenderHasNoRightToVote();
-    //     if (voters[msg.sender].voted) revert SenderAlreadyVoted();
+    function vote(uint256[] calldata vote_) external {
+        require(_voters[msg.sender].weight > 0);
+        require(!_voters[msg.sender].voted);
+        require(vote_.length <= 3);
+        require(_active);
 
-    //     votes.push(Vote({
-    //         voter: msg.sender,
-    //         vote1: ballot[0],
-    //         vote2: ballot[1],
-    //         vote3: ballot[2]
-    //     }));
+        // add vote to vote tree
+        addVote(vote_);
 
-    //     voters[msg.sender].voted = true;
-    //     totalVotes += 1;
-    // }
+        // add vote to voter's record
+        uint256[] storage votes = _voters[msg.sender].votes;
+        for (uint256 i = 0; i < vote_.length; i++) {
+            votes.push(vote_[i]);
+        }
+        _voters[msg.sender].voted = true;
+    }
 
-    // Calculate droop quote
-    // function droopQuota() private view returns (uint256) {
-    //     return (totalVotes - 1) / (proposals.length + 1) + 1; // Division rounds down to nearest int.
-    // }
+    function getVote() external view returns (uint256[] memory votes) {
+        return _voters[msg.sender].votes;
+    }
 
-    // Tally winners
-    // function tallyWinners() public view {
-    //     if (msg.sender != chairperson) revert OnlyChairperson();
+    function end(uint256 numWinners) external returns (uint256[] memory winners_) {
+        require(msg.sender == chairperson);
+        require(_active);
 
-    //     uint256 quota = droopQuota();
-    // }
+        winners_ = finalize(numWinners);
+        for (uint256 i = 0; i < winners_.length; i++) {
+            _winners.push(winners_[i]);
+        }
+
+        _active = false;
+
+        return winners_;
+    }
+
+    function winners() external view returns (uint256[] memory winners_) {
+        require(!_active);
+
+        for (uint256 i = 0; i < _winners.length; i++) {
+            winners_[i] = _winners[i];
+        }
+
+        return winners_;
+    }
 
 }
