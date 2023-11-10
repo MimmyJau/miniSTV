540f26a0d9044a6ecb01e2ec64a1ca37d3810cbd
Jimmy <jimmy.b.mau@gmail.com>
2023-11-10 15:16:16 -0500

    test: add passing tests for voting

--- test/StvExample.t.sol
+++ test/StvExample.t.sol
@@ -92,25 +92,25 @@ contract Suffrage is Test {
         stvE.suffrage(testAddr);
         stvE.start();
 
-        uint256[] memory ballot = new uint256[](3);
-        ballot[0] = 4;
-        ballot[1] = 2;
-        ballot[2] = 1;
+        uint256[] memory vote = new uint256[](3);
+        vote[0] = 4;
+        vote[1] = 2;
+        vote[2] = 1;
 
         vm.prank(testAddr);
-        stvE.vote(ballot);
+        stvE.vote(vote);
     }
 
     function testFail_votingWithSuffrageBeforeStart() public {
         stvE.suffrage(testAddr);
 
-        uint256[] memory ballot = new uint256[](3);
-        ballot[0] = 4;
-        ballot[1] = 2;
-        ballot[2] = 1;
+        uint256[] memory vote = new uint256[](3);
+        vote[0] = 4;
+        vote[1] = 2;
+        vote[2] = 1;
 
         vm.prank(testAddr);
-        stvE.vote(ballot);
+        stvE.vote(vote);
     }
 
     function testFail_votingWithSuffrageAfterEnd() public {
@@ -118,36 +118,145 @@ contract Suffrage is Test {
         stvE.start();
         stvE.end(1);
 
-        uint256[] memory ballot = new uint256[](3);
-        ballot[0] = 4;
-        ballot[1] = 2;
-        ballot[2] = 1;
+        uint256[] memory vote = new uint256[](3);
+        vote[0] = 4;
+        vote[1] = 2;
+        vote[2] = 1;
 
         vm.prank(testAddr);
-        stvE.vote(ballot);
+        stvE.vote(vote);
     }
 
     function testFail_votingWithoutSuffrage() public {
         stvE.start();
 
-        uint256[] memory ballot = new uint256[](3);
-        ballot[0] = 4;
-        ballot[1] = 2;
-        ballot[2] = 1;
+        uint256[] memory vote = new uint256[](3);
+        vote[0] = 4;
+        vote[1] = 2;
+        vote[2] = 1;
 
         vm.prank(testAddr);
-        stvE.vote(ballot);
+        stvE.vote(vote);
     }
 
     function testFail_votingWithoutSuffrageBeforeStart() public {
-        uint256[] memory ballot = new uint256[](3);
-        ballot[0] = 4;
-        ballot[1] = 2;
-        ballot[2] = 1;
+        uint256[] memory vote = new uint256[](3);
+        vote[0] = 4;
+        vote[1] = 2;
+        vote[2] = 1;
 
         vm.prank(testAddr);
-        stvE.vote(ballot);
+        stvE.vote(vote);
     }
 }
+
+contract Vote is Test {
+    StvExample public stvE;
+    address alice;
+    address bob;
+    address carol;
+
+
+    function setUp() public {
+        stvE = new StvExample();
+
+        // test proposals
+        bytes[] memory proposals_ = new bytes[](4);
+        proposals_[0] = "apple";
+        proposals_[1] = "orange";
+        proposals_[2] = "banana";
+        proposals_[3] = "mango";
+        stvE.addProposals(proposals_);
+
+        // test address
+        alice = address(bytes20(keccak256(abi.encode("alice"))));
+        bob = address(bytes20(keccak256(abi.encode("bob"))));
+        carol = address(bytes20(keccak256(abi.encode("carol"))));
+
+        stvE.suffrage(alice);
+        stvE.suffrage(bob);
+        stvE.suffrage(carol);
+
+        stvE.start();
+    }
+
+    function test_votesFromThreeUsers() public {
+        uint256[] memory vote = new uint256[](3);
+        vote[0] = 4;
+        vote[1] = 2;
+        vote[2] = 1;
+
+        vm.prank(alice);
+        stvE.vote(vote);
+
+        vote[0] = 1;
+        vote[1] = 3;
+        vote[2] = 2;
+
+        vm.prank(bob);
+        stvE.vote(vote);
+
+        vote[0] = 3;
+        vote[1] = 4;
+        vote[2] = 1;
+
+        vm.prank(carol);
+        stvE.vote(vote);
+    }
+
+    function testFail_sameUserVotesTwice() public {
+        uint256[] memory vote = new uint256[](3);
+        vote[0] = 4;
+        vote[1] = 2;
+        vote[2] = 1;
+
+        vm.prank(alice);
+        stvE.vote(vote);
+
+        vote[0] = 1;
+        vote[1] = 3;
+        vote[2] = 2;
+
+        vm.prank(alice);
+        stvE.vote(vote);
+    }
+
+    function test_submitVoteWithOneChoice() public {
+        uint256[] memory vote = new uint256[](1);
+        vote[0] = 4;
+
+        vm.prank(alice);
+        stvE.vote(vote);
+    }
+
+    function test_submitVoteWithTwoChoices() public {
+        uint256[] memory vote = new uint256[](2);
+        vote[0] = 4;
+        vote[1] = 3;
+
+        vm.prank(bob);
+        stvE.vote(vote);
+    }
+
+    function testFail_submitVoteWithFourChoices() public {
+        uint256[] memory vote = new uint256[](4);
+        vote[0] = 4;
+        vote[1] = 3;
+        vote[2] = 2;
+        vote[3] = 1;
+
+        vm.prank(carol);
+        stvE.vote(vote);
+    }
+
+    function testFail_submitVoteWithDuplicateChoices() public {
+        uint256[] memory vote = new uint256[](3);
+        vote[0] = 4;
+        vote[1] = 3;
+        vote[2] = 4;
+
+        vm.prank(bob);
+        stvE.vote(vote);
+    }
 
 }
