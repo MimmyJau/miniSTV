0ee6388bba0b1eaafad918c174efa4006664b3cb
Jimmy <jimmy.b.mau@gmail.com>
2023-11-14 13:53:31 -0500

    docs: add NatSpec for Tree.sol file

--- src/Tree.sol
+++ src/Tree.sol
@@ -6,7 +6,7 @@ uint256 constant MAX_DEPTH = 3;
 struct Node {
     bytes32 id; // Key of current node.
     bytes32 parent; // Key of parent node.
-    bytes32[] children; // Key of child nodes.
+    bytes32[] children; // Keys of child nodes.
 }
 
 contract Tree {
@@ -17,30 +17,38 @@ contract Tree {
         tree[root].id = root;
     }
     
+    /// @dev Generates a new and unique key (or address) for a child node
     function generateId(bytes32 parent) private view returns (bytes32) {
         uint index = tree[parent].children.length;
         uint time = block.timestamp;
         return keccak256(abi.encode(parent, index, time));
     }
 
+    /// @dev Adds a child node to the parent node
     function addChild(bytes32 parent) internal returns (bytes32) {
-        // Generate new key for child node.
+
+        // generate new key for child node
         bytes32 newChild = generateId(parent);
         tree[parent].children.push(newChild);
-        // Instantiate new child node.
+
+        // instantiate new child node
         tree[newChild].parent = parent;
         tree[newChild].id = newChild;
+
         return newChild;
     }
 
+    /// @dev Returns an array of keys of the child nodes
     function getChildren(bytes32 parent) internal view returns (bytes32[] memory) {
         return tree[parent].children;
     }
 
+    /// @dev Returns the key of the parent node
     function getParent(bytes32 node) internal view returns (bytes32) {
         return tree[node].parent;
     }
 
+    /// @dev Returns the key of the root node of the tree
     function getRoot() internal view returns (bytes32) {
         return root;
     }
